c2e10859d5f33abc554fbb0e5184bd33f36fabb1
Russell Jarvis <rjjarvis@asu.edu>
2019-07-11 02:57:31 +0000

    before merge

--- entry_point.py
+++ entry_point.py
@@ -20,11 +20,11 @@ import glob
 # The faster way to complete everything when confident
 ##
 
-#runnable_nml.faster_make_model_and_cache()
-#file_paths = glob.glob("models/*.p")
-#_ = runnable_nml.analyze_models_from_cache(file_paths[0:2])
+runnable_nml.faster_make_model_and_cache()
+file_paths = glob.glob("models/*.p")
+_ = runnable_nml.analyze_models_from_cache(file_paths)
 
-#runnable_allen.faster_run_on_allen(100)
+runnable_allen.faster_run_on_allen(150)
 
 file_paths = glob.glob("three_feature_folder/*.p")
 nml_data = []
@@ -37,5 +37,15 @@ file_paths = glob.glob("allen_three_feature_folder/*.p")
 allen_analysis = []
 for f in file_paths:
     allen_analysis.append(pickle.load(open(f,'rb')))
-merged = runnable_nml.giant_frame(allen_analysis,nml_data,onefive=True,other_dir='second_dir')
-merged = runnable_nml.giant_frame(allen_analysis,nml_data,onefive=False,other_dir='second_dir')
+#import pdb
+#pdb.set_trace()
+path=str('new_dir')
+
+try:
+    import os
+    os.mkdir(path)
+except:
+    pass
+merged = runnable_nml.giant_frame(allen_analysis,nml_data,onefive=True,other_dir=path)
+merged = runnable_nml.giant_frame(allen_analysis,nml_data,onefive=False,other_dir=path)
+print('so it finished')
