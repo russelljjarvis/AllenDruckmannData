931eb4bfcf93049631c10b32ccfd1a16d09d3717
Russell <russelljarvis@protonmail.com>
2020-08-24 08:24:24 +1000

    for thesis

---
+++ allen_nu_tests/make_allen.py
@@ -0,0 +1,67 @@
+from make_allen_tests import AllenTest
+nml_data = []
+import pickle
+import glob
+from sciunit import TestSuite
+from neuronunit.optimisation.optimization_management import TSD
+
+#file_paths = glob.glob("three_feature_folder/*.p")
+#for f in file_paths:
+#ickle   nml_data.append(pickle.load(open(f,'rb')))
+file_paths = glob.glob("allen_three_feature_folder/*.p")
+allen_analysis = []
+allen_test_suites = []
+cnt = 0
+for f in file_paths:
+    contents = pickle.load(open(f,'rb'))
+    cnt+=1
+    allen_tests = []
+    #print(contents['model_information'],contents.keys())
+    if contents['allen_15'] is not None:
+        for k,v in contents['allen_15'].items():
+            at = AllenTest(name=str(k)+'_1.5x')
+            at.set_prediction(v)
+            at.set_observation(v)
+            score = at.feature_judge()
+            print(score)
+            allen_tests.append(at)
+    if contents['allen_30'] is not None:
+        for k,v in contents['allen_30'].items():
+            at = AllenTest(name=str(k)+'_3.0x')
+            at.set_prediction(v)
+            at.set_observation(v)
+            score = at.feature_judge()
+            print(score)
+            allen_tests.append(at)
+
+    if contents['efel_15'] is not None:
+        for k,v in contents['efel_15'][0].items():
+            at = AllenTest(name=str(k)+'_1.5x')
+            at.set_observation(v)
+            allen_tests.append(at)
+    if contents['efel_30'] is not None:
+        for k,v in contents['efel_30'][0].items():
+            at = AllenTest(name=str(k)+'_3.0x')
+            at.set_observation(v)
+            allen_tests.append(at)
+    if contents['dm'] is not None:
+        for k,v in contents['dm'].items():
+            at = AllenTest(name='dm'+str(k))
+            at.set_observation(v)
+            allen_tests.append(at)
+        
+    #, 'efel_30', 'allen_30', 'model_id', 'model_information', 'dm', 'allen_15'])   
+    #print(contents.keys())
+    suite = TestSuite(allen_tests,name=str(contents['model_id']))
+    allen_test_suites.append(suite)
+    
+    print(TSD(suite))
+    pickle.dump(allen_test_suites,open('allen_NU_tests.p','wb'))
+    print('made tests')
+    if cnt == 10:
+        break
+
+    
+
+
+    
